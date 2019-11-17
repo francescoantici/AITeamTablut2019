ccc30e744e092b8d8beb34c727b542a9371c2a39
francescoantici <francesco.antici98@gmail.com>
2019-11-17 12:09:04 +0100

    ki

--- game/State.py
+++ game/State.py
@@ -1,4 +1,11 @@
 class State:
     def __init__(self, playboard):
-        self.turn = 0 #0 = WHITE, 1 = BLACK
-        self.playboard = init_state
+        self.__turn = 0 #0 = WHITE, 1 = BLACK
+        self.__playboard = playboard
+    
+    def to_move(self):
+        if self.turn:
+            self.turn=0
+        else:
+            self.turn=1
+        return self.turn
