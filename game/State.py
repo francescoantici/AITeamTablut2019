44cfe6e2b09911170edfbd2f0b42066fea785100
francescoantici <francesco.antici98@gmail.com>
2019-11-17 13:17:51 +0100

    white moves

--- game/State.py
+++ game/State.py
@@ -1,7 +1,8 @@
 class State:
     def __init__(self, playboard):
-        self.__turn = 0 #0 = WHITE, 1 = BLACK
-        self.__playboard = playboard
+        self.turn = 0 #0 = WHITE, 1 = BLACK
+        self.playboard = playboard
+        self.camps=[(0,3),(0,4),(0,5),(1,4),(3,0),(4,0),(5,0),(4,1),(4,7),(3,8),(4,8),(5,8),(7,4),(8,3),(8,4),(8,5)]
     
     def to_move(self):
         if self.turn:
--- game/gamedef.py
+++ game/gamedef.py
@@ -2,21 +2,45 @@ import sys
 sys.path.append("/Users/francesco/Documents/Fundamentals/tablut/Tablut2019/lib")
 import games4e
 import numpy as np
+import State
 
 stato=np.zeros((10,9))
 class MyGame(games4e.Game):
     "The state in this game would be managed as a numpy 2-D array"
     def __init__(self, state):
-        self.initial = state
+        self.initial = state.playboard
     def actions(self,state):
         L=[]
-        camps=[(0,3),(0,4),(0,5),(1,4),(3,0),(4,0),(5,0),(4,1),(4,7),(3,8),(4,8),(5,8),(7,4),(8,3),(8,4),(8,5)]
-        for i in range(state.__playboard.shape[0]):
-            for j in range(state.__playboard.shape[1]):
-                if state.__playboard[i,j] == 0:
+        for i in range(state.playboard.shape[0]):
+            for j in range(state.playboard.shape[1]):
+                if state.playboard[i,j] == 0:
                     continue
-                elif state.__playboard[i,j] > 0:
-
+                elif state.playboard[i,j] > 0:
+                    for k in range(state.playboard.shape[0]):
+                        #check if the poition is valid by avoiding considering moves into camps, other checkers position and the starting one
+                        if (k,j)==(i,j) or (k,j) in state.camps or state.playboard[k,j] != 0: 
+                            continue
+                        else:
+                                L.append((i,j,k,j))
+                    for q in range(state.playboard.shape[1]):
+                        if (i,q)==(i,j) or (i,q) in state.camps or state.playboard[i,q] != 0: 
+                            continue
+                        else:
+                                L.append((i,j,i,q))
                     
                 else:
                     continue #black actions
+        return L
+
+p=np.array([[0,0,0,-1,-1,-1,0,0,0],
+            [0,0,0,0,-1,0,0,0,0],
+            [0,0,0,0,1,0,0,0,0],
+            [-1,0,0,0,1,0,0,0,-1],
+            [-1,-1,1,1,2,1,1,-1,-1],
+            [-1,0,0,0,1,0,0,0,-1],
+            [0,0,0,0,1,0,0,0,0],
+            [0,0,0,0,-1,0,0,0,0],
+            [0,0,0,-1,-1,-1,0,0,0]])
+s=State.State(p)
+g=MyGame(s)
+print(g.actions(s))
