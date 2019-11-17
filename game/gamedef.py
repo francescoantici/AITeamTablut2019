03b63b631fcfedd06105606124f822fcdc81b813
francescoantici <francesco.antici98@gmail.com>
2019-11-17 12:24:02 +0100

    ijij

--- game/gamedef.py
+++ game/gamedef.py
@@ -8,5 +8,15 @@ class MyGame(games4e.Game):
     "The state in this game would be managed as a numpy 2-D array"
     def __init__(self, state):
         self.initial = state
-    
+    def actions(self,state):
+        L=[]
+        camps=[(0,3),(0,4),(0,5),(1,4),(3,0),(4,0),(5,0),(4,1),(4,7),(3,8),(4,8),(5,8),(7,4),(8,3),(8,4),(8,5)]
+        for i in range(state.__playboard.shape[0]):
+            for j in range(state.__playboard.shape[1]):
+                if state.__playboard[i,j] == 0:
+                    continue
+                elif state.__playboard[i,j] > 0:
 
+                    
+                else:
+                    continue #black actions
