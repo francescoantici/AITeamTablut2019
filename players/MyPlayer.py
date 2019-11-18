957901ba699a0101314335f0b64333183195a22e
francescoantici <francesco.antici98@gmail.com>
2019-11-18 18:08:32 +0100

    tot

--- players/MyPlayer.py
+++ players/MyPlayer.py
@@ -1,6 +1,7 @@
 from players.Player import Player
 from game.TablutAshtonGame import TablutAshtonGame
 import numpy as np
+from game.ChessBoard import ChessBoard
 
 inf = float('inf')
 class MyPlayer(Player):
@@ -9,20 +10,32 @@ class MyPlayer(Player):
 
         
     def euristic(self,chessboard):
-        return np.random.uniform(0,1)
+        
+        k=tuple(np.where(chessboard==2).reshape(2))
+        if (k[0] >= 3 and k[0] <= 5) and (k[1] >= 3 and k[1] <= 5):
+            return 2
+        elif k in Chessboard.EXIT_INDICES:
+            return 0
+        else:
+            return 1
+
+
+
+        
+        
 
     def alphabeta_cutoff(self,game,opponent, d=4, cutoff_test=None):
         """Search game to determine best action; use alpha-beta pruning.
         This version cuts off search and uses an evaluation function."""
 
     
-    
+        current=self
         # Functions used by alphabeta
         def max_value(state, alpha, beta, depth):
             if cutoff_test(state, depth):
-                return self.euristic(state.getPawnBoard())
+                return current.euristic(state.getBoard())
             v = -inf
-            for a in self.actions(state):
+            for a in current.actions(state):
                 v = max(v, min_value(state.result(*a),
                                     alpha, beta, depth + 1))
                 if v >= beta:
@@ -32,7 +45,7 @@ class MyPlayer(Player):
 
         def min_value(state, alpha, beta, depth):
             if cutoff_test(state, depth):
-                return self.euristic(state.getPawnBoard())
+                return current.euristic(state.getBoard())
             v = inf
             for a in opponent.actions(state):
                 v = min(v, max_value(state.result(*a),
@@ -50,8 +63,8 @@ class MyPlayer(Player):
         best_score = -inf
         beta = inf
         best_action = None
-        for a in game.actions(state):
-            v = min_value(game.result(*a), best_score, beta)
+        for a in current.actions(game):
+            v = min_value(game.result(*a), best_score, beta,1)
             if v > best_score:
                 best_score = v
                 best_action = a
